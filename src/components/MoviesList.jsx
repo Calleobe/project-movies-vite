88fa5cf06356dfa0e3a086ab649571cd00985cd0
Calleobe <145662831+Calleobe@users.noreply.github.com>
2023-11-09 10:10:40 +0100

    Added dropdown

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,35 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import '../css/MoviesList.css'
+import "../css/MoviesList.css";
 
 const MoviesList = ({ apiKey }) => {
   const [movies, setMovies] = useState([]);
+  const [listType, setListType] = useState("popular"); // New state for list type
+
+  // Object mapping list types to their API endpoints
+  const apiEndpoints = {
+    popular: "popular",
+    now_playing: "now_playing",
+    top_rated: "top_rated",
+    upcoming: "upcoming",
+  };
 
   useEffect(() => {
     const fetchMovies = async () => {
       const response = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+        `https://api.themoviedb.org/3/movie/${apiEndpoints[listType]}?api_key=${apiKey}&language=en-US&page=1`
       );
       const data = await response.json();
       setMovies(data.results);
     };
 
     fetchMovies();
-  }, [apiKey]);
+  }, [apiKey, listType]); // Adding listType as a dependency
 
   return (
-    <div className="MoviesList">
-      {movies.map((movie) => (
-        <Link key={movie.id} to={`/movies/${movie.id}`}>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-          />
-          <div className="MovieDetails">
-            <h2>{movie.title}</h2>
-          </div>
-        </Link>
-      ))}
+    <div>
+      <select
+        value={listType}
+        onChange={(e) => setListType(e.target.value)}
+        className="movie-list-dropdown"
+      >
+        <option value="popular">Popular</option>
+        <option value="now_playing">Now Playing</option>
+        <option value="top_rated">Top Rated</option>
+        <option value="upcoming">Upcoming</option>
+      </select>
+      <div className="MoviesList">
+        {movies.map((movie) => (
+          <Link key={movie.id} to={`/movies/${movie.id}`}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <div className="MovieDetails">
+              <h2>{movie.title}</h2>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
